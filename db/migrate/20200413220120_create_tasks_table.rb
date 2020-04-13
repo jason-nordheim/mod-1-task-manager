af60874115e6b05e33448c46999eb871e62d3442
jason-nordheim <jason.nordheim@gmail.com>
2020-04-13 18:21:22 -0400

    fixed first migration

--- db/migrate/20200413220120_create_tasks_table.rb
+++ db/migrate/20200413220120_create_tasks_table.rb
@@ -1,11 +1,12 @@
 class CreateTasksTable < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t| 
-      t.string    :name           null: false # name field should never be false 
-      t.text      :description    null: true  # description can be empty 
-      t.datetime  :due            null: true  # due date can be null 
+      t.string    :name    
+      t.text      :description
+      t.datetime  :due
 
       # created_at, updated_at 
       t.timestamps  
     end
+  end 
 end
